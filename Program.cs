8edbde82d592cea60a10b9fec6a7a32e300d4fdf
mabbuprem <mabbupremkumar@gmail.com>
2022-03-11 11:08:28 +0530

    As a Calculative Gambler stake 50%

--- Program.cs
+++ Program.cs
@@ -15,24 +15,31 @@ namespace GamblerSimulation
         public static void PlayGame()
         {
 
-            const int StartingStake = 100;
-            const int BetPerGame = 1;
+            const int STARTING_STAKE = 100;
+            const int BET_PER_GAME = 1;
+            const int WIN = 1;
+            const int LOOSE = 0;
+            int NoOfBets = 0;
 
-            Gamblers gambler1 = new Gamblers(StartingStake);
+            Gamblers gambler1 = new Gamblers(STARTING_STAKE);
             int Gamblers_Stake = gambler1.Gamblers_stake;
-
-            Random random = new Random();
-            int WinOrLoose = random.Next(0, 2);
-            switch (WinOrLoose)
+            while (Gamblers_Stake > 50 && Gamblers_Stake < 150)
             {
-                case 0:
-                    Gamblers_Stake = Gamblers_Stake - BetPerGame;
-                    break;
-                case 1:
-                    Gamblers_Stake = Gamblers_Stake + BetPerGame;
-                    break;
+                Random random = new Random();
+                int WinOrLoose = random.Next(0, 2);
+                switch (WinOrLoose)
+                {
+                    case LOOSE:
+                        Gamblers_Stake = Gamblers_Stake - BET_PER_GAME;
+                        break;
+                    case WIN:
+                        Gamblers_Stake = Gamblers_Stake + BET_PER_GAME;
+                        break;
+                    default:
+                        break;
+                }
+                NoOfBets++;
             }
-
             Console.WriteLine(Gamblers_Stake);
 
         }
