b58d927c4b9cd9b8b5d62385d82abc18558840e1
mabbuprem <mabbupremkumar@gmail.com>
2022-03-11 09:19:49 +0530

    Gambler would start with a stake

---
+++ Gambler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerSimulation
+{
+    public class Gamblers
+    {
+        public int Gamblers_stake;
+        internal int Gamlers_stake;
+
+        public Gamblers(int gamblers_stake)
+
+        {
+            Gamblers_stake = gamblers_stake;
+        }
+
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -4,11 +4,22 @@ namespace GamblerSimulation
 {
     internal class Program
     {
+        private int Gamblers_Stake;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Gambler Simulation");
-            
-            
+
+        }
+
+        public void OlayGame()
+        {
+            Gamblers gambler1 = new Gamblers(100);
+            int Gambler_Stake = gambler1.Gamlers_stake;
+            for (int day = 0; Gambler_Stake >= 0; day++)
+            {
+                Gamblers_Stake--;
+            }
         }
     }
 }
